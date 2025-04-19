a8d56b2a7e1dfe2b51477a79f308590494ebcdf5
fateEight <bsnpardhu@gmail.com>
2025-04-19 21:25:12 +0530

    Update robotic_arm_path_follow.m

--- robotic_arm_path_follow.m
+++ robotic_arm_path_follow.m
@@ -2,9 +2,7 @@ function robotic_arm_path2_follow(points)
     L1 = 10;
     L2 = 10;
     steps_per_segment = 50;
-    max_reach = L1 + L2;
-    valid_indices = sqrt(points(:,1).^2 + points(:,2).^2) <= max_reach;
-    points = points(valid_indices, :);
+    points = points(sqrt(points(:,1).^2 + points(:,2).^2) <= L1 + L2, :);
     if size(points, 1) < 2
         error('Not enough valid points to form a path');
     end
@@ -23,15 +21,7 @@ function robotic_arm_path2_follow(points)
             x = x_segment(j);
             y = y_segment(j);
             r = sqrt(x^2 + y^2);
-            if r > max_reach || r < abs(L1-L2)
-                warning('Point (%f, %f) is not reachable', x, y);
-                continue;
-            end
             cos_theta2 = (r^2 - L1^2 - L2^2) / (2 * L1 * L2);
-            if abs(cos_theta2) > 1
-                warning('Point (%f, %f) is not reachable (invalid cos_theta2)', x, y);
-                continue;
-            end
             theta2 = atan2(-sqrt(1 - cos_theta2^2), cos_theta2);
             beta = atan2(y, x);
             gamma = atan2(L2 * sin(theta2), L1 + L2 * cos(theta2));
@@ -48,8 +38,8 @@ function robotic_arm_path2_follow(points)
             plot(x1, y1, 'ko', 'MarkerSize', 8, 'MarkerFaceColor', 'k');
             plot(x2, y2, 'go', 'MarkerSize', 6, 'MarkerFaceColor', 'g');
             plot(x, y, 'mo', 'MarkerSize', 8, 'MarkerFaceColor', 'm');
-            xlim([-20, 20]);
-            ylim([-20, 25]);
+            xlim([-10, 10]);
+            ylim([-10, 10]);
             text(-18, 20, sprintf('θ1: %.1f°, θ2: %.1f°', theta1*180/pi, theta2*180/pi));
             err = sqrt((x2-x)^2 + (y2-y)^2);
             text(-18, 18, sprintf('Error: %.2f units', err));
